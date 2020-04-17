8ea7dec0ef8196aa80e87a47663d23d4a06ed923
magvtv <magutujr@gmail.com>
2020-04-17 13:08:34 +0300

    updated js  scripts with click fuction to thepitza pitza

--- js/myScripts.js
+++ js/myScripts.js
@@ -43,31 +43,83 @@ Pitza.prototype.calculateBei = function () {
 
         for (var i = 0; i < this.crust.length; i++) {
             if (this.crust[i] === "thin") {
-                bei += 100
+                bei += 50
             }
 
             if (this.crust[i] === "thick") {
-                bei += 150
+                bei += 100
             }
 
             if (this.crust[i] === "deep") {
-                bei += 200
+                bei += 150
             }
 
             if (this.crust[i] === "mozzarella filled") {
-                bei += 250
+                bei += 200
             }
 
             if (this.crust[i] === "stuffed") {
-                bei += 170
+                bei += 100
             }
 
             if(this.crust[i] === "glutten free") {
-                bei += 100
+                bei += 50
             }
         }
     }
     return bei;
  }
 
- 
+ $(document).ready(function(){
+    $("#sizeButton").click(funtion(){
+        var size = $("input [name = pitzaSize]:checked").val();
+
+        if (!size) {
+            alert("Hujachagua size yako bwana!");
+        } else {
+            $(".toppings").slideDown();
+            $(".size").slideUp();
+
+        }
+        
+    })
+});
+
+$(document).ready(function(){
+    $("#toppingsButton").click(function(){
+        var toppings =$("input [name = pitzaToppings]:checked").val();
+
+        if (!toppings) {
+            alert("Hujachagua toppings bwana!");
+        } else {
+            $(".crust").slideDown();
+            $(".toppings").slideUp();
+        }
+    });
+
+$(document).ready(function() {
+    $("#crust").click(function() {
+        var crust = $("input [name = pitzaCrust]:checked").val();
+
+        if (!crust) {
+            alert("Hujachagua crust bwana!");
+
+         } else {
+             $(".quantity").slideDown();
+             $(".crust").slideUp();
+         }
+    })
+});
+
+$("#deLiver").click(function() {
+    var deliver = $("input [name = deliveryOptions]:selected").val();
+
+    if (!deliver) {
+        alert("Unataka sasa ikufikie aje mtu wangu!");
+    } else {
+        $("#d1").text(deliver);
+        $("#d2").show();
+        $(".deliver").slideUp();
+    }
+})
+
